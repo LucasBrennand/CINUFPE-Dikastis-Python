b1280d48cf954e367ca1ea4849e42a5dff9406d0
lucas <lucasbrennand.barbosa@gmail.com>
2026-05-13 22:22:33 -0300

    Finish aula 6

--- Aula 6/atv_3.py
+++ Aula 6/atv_3.py
@@ -1,38 +1,39 @@
 resistencia = 6
-total_itens = 0
 entrada = input()
 
 def mover(resistencia, entrada, total_itens):
-    resistencia -= 1
     entrada = entrada.split(' ')
     movimento_atual = entrada[0]
     if resistencia <= 0:
         print('A correnteza está muito forte... não consigo continuar.')
         return -1
     elif movimento_atual.lower() == "linguado":
-        resistencia += 2
+        resistencia += 1
         print('Obrigada, Linguado! Vamos rápido!')
     elif movimento_atual.lower() == "polvo":
-        resistencia -= 2
+        resistencia -= 3
         print('Cuidado com os servos da bruxa!')
     elif movimento_atual == '~':
         movimento_atual = ''
-    elif int(movimento_atual) >= 0:
-        total_itens += int(movimento_atual)
+        resistencia -= 1
     else:
-        movimento_atual = ''
+        total_itens += int(movimento_atual)
+        resistencia -= 1
     entrada = entrada[1:]
     # print(movimento_atual)
     # print(entrada)
-    if (resistencia == 0):
-        print('A correnteza está muito forte... não consigo continuar.')
-        print('O príncipe afundou... Úrsula venceu desta vez.')
+    if (resistencia <= 0):
         return -1
     elif len(entrada) == 0:
-        print(f'Eric foi salvo! E Ariel ainda guardou {total_itens} bugigangas na sua gruta.')
-        return -1
+        return total_itens
     else:
         entrada = ' '.join(entrada)
-        mover(resistencia, entrada, total_itens)
+        return mover(resistencia, entrada, total_itens)
     
-mover(resistencia, entrada, total_itens=0)
+total_itens = mover(resistencia, entrada, total_itens=0)
+if total_itens > 0:
+    print(f'Eric foi salvo! E Ariel ainda guardou {total_itens} bugigangas na sua gruta.')
+else:
+    print('A correnteza está muito forte... não consigo continuar.')
+    print('O príncipe afundou... Úrsula venceu desta vez.')
+    
